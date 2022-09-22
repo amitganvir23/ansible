40b3562e861daaba876da648294811839bd7d09a
Open Source book <amitganvir6@gmail.com>
2022-09-23 01:08:03 +0530

    Update iis_version_check.ps1

--- windows/iis_version_check.ps1
+++ windows/iis_version_check.ps1
@@ -1,15 +1,16 @@
 $iisInfo = Get-ItemProperty HKLM:\SOFTWARE\Microsoft\InetStp\
 $version = [decimal]"$($iisInfo.MajorVersion).$($iisInfo.MinorVersion)"
-echo $version
+#echo $version
 $iisversion=$version
-$iisversion=4
+#$iisversion=4
 Write-Host iisversion = $iisversion
 If($iisversion -like '*IIS 8*'){
     Write-Host This server uses IIS6
 }
 
 If($iisversion -gt 6){
-    Write-Host This server uses IIS-5 laterversion
+    #Write-Host This server uses IIS-5 laterversion
+    Write-Host 
 }
 Else
 {
