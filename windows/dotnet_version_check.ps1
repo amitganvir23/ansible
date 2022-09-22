be66d7fb8f759159888bbe12f9afef253758318c
Open Source book <amitganvir6@gmail.com>
2022-09-23 02:08:54 +0530

    Create dotnet_version_check.ps1

---
+++ windows/dotnet_version_check.ps1
@@ -0,0 +1,24 @@
+#Get-ItemProperty "HKLM:\SOFTWARE\Microsoft\NET Framework Setup\NDP\v4\Full"
+#https://learn.microsoft.com/en-us/dotnet/framework/migration-guide/versions-and-dependencies
+
+$dotnetInfo = Get-ItemProperty "HKLM:\SOFTWARE\Microsoft\NET Framework Setup\NDP\v4\Full"
+$version = "$($dotnetInfo.Version)"
+$release = "$($dotnetInfo.Release)"
+$min_version=$args[0]
+#echo current: $version 
+#echo $min_version
+#$dotnetversion=$version
+$dotnetversion=$release
+
+Write-Host dotnetversion = $dotnetversion
+
+If($dotnetversion -gt $min_version)
+{
+    Write-Host Current DOT Net Framework version is $version
+}
+Else
+{
+   #Write-Host DOT Net Framework having Lower Version
+   throw "Error Message: DOT Net Framework having Lower Version"
+
+}
